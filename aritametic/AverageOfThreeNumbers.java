c311cf531c608a69efdc04521d5aa06f0d3c3ecd
yashjagdale0 <jagdaleyash23@gmail.com>
2026-08-22 20:35:44 +0530

    Merged changes

---
+++ aritametic/AverageOfThreeNumbers.java
@@ -0,0 +1,7 @@
+package aritametic;
+
+public class AverageOfThreeNumbers {
+    int a = 10, b = 20, c = 30;
+        double avg = (a + b + c) / 3.0;
+        System.out.println("Average = " + avg);
+}
---
+++ aritametic/AverageOfTwoNumbers.java
@@ -0,0 +1,9 @@
+package aritametic;
+
+class AverageOfTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        double avg = (a + b) / 2.0;
+        System.out.println("Average = " + avg);
+    }
+}
